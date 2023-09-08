0c9c234a35de03f70e490b50b9a49abbd803846b
Dumnersm580 <80608783+Dumnersm580@users.noreply.github.com>
2023-09-08 18:04:17 -0400

    fixed some issues

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+
+namespace RestInventorySystem.Platforms.MacCatalyst;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp()
+	{
+		return MauiProgram.CreateMauiAppAsync();
+	}
+}
